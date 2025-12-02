c51a81dc998180a8f3ac02ddfd88cc15d2aa942a
Nola <Nola.Bloop@gmail.com>
2025-12-02 15:38:30 -0500

    ...

--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -5,26 +5,31 @@ const router = express.Router();
 
 // ?name&userId
 router.post("/", (req, res) => {
+  console.log(req.originalUrl)
   controller.Create(req, res)
 });
 
 // /user/:userId
 router.get("/user/:userId", (req, res) => {
+  console.log(req.originalUrl)
   controller.ReadUser(req, res)
 });
 
 // /id/:id
 router.get("/id/:id", (req, res) => {
+  console.log(req.originalUrl)
   controller.ReadId(req, res)
 });
 
 // ?userId&id[&name][&count][&total_time][&author]
 router.put("/", (req, res) => {
+  console.log(req.originalUrl)
   controller.Create(req, res)
 });
 
 // ?userId&id
 router.delete("/", (req, res) => {
+  console.log(req.originalUrl)
   controller.Delete(req, res)
 });
 
